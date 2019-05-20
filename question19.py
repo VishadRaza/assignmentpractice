e1926e6fa79e14f783d1368312f47fb73d0e05a5
Vishad <syedavishadrazazaidi@gmail.com>
2019-05-20 22:26:51 +0500

    distance in miles,inches and yards

---
+++ question19.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter feet"))
+
+inches = num1 * 12
+yards = num1/3
+miles = num1/5280
+
+print("distance in inches ",round(inches,1))
+print("distance in yards ",round(yards,1))
+print("distance in miles ",round(miles,4))
