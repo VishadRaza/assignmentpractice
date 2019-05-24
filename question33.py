ff6a0fd15d6c846ff056e0454d55d651b909c148
Vishad <syedavishadrazazaidi@gmail.com>
2019-05-24 23:45:50 +0500

    Reverse of full name

--- question33.py
+++ question33.py
@@ -1,16 +1,6 @@
-first_name = input("enter your first name")
-last_name = input("enter your last name")
 a = ""
-b = ""
-# z = first_name[::-1]
-# k = last_name[::-1]
+name = input("enter your name")
+for i in range(1,len(name)+1):
+      a += name[len(name) - i]
+print(a)
 
-for i in range(1,len(first_name)+1):
-    a += first_name[len(first_name) - i]
-
-for j in range(1,len(last_name)+1):
-    b += last_name[len(first_name) - j]
-
-
-
-print("{} {}".format(a,b))
